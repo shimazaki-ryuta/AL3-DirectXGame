a7c79b7ce299cc266ad90c9ffa253d5b46a7a577
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2023-04-17 14:46:57 +0900

    ブランチ

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2A_07_シマザキ_リュウタ_AL3");
+	win->CreateGameWindow();
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
